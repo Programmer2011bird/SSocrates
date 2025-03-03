774c945a2de4c6e6f69c9c5c091dd02b99a69b72
Programmer2011bird <maysamshaker7@gmail.com>
2025-03-03 21:11:22 +0330

    comleted the searching part

--- search-engine/searcher.py
+++ search-engine/searcher.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+from requests.exceptions import SSLError
+
 
 def parse_json(json_file:str) -> list[dict[str,str]]:
     with open(json_file, "r+") as file:
@@ -22,13 +24,29 @@ def search_sites_by_category(category: str) -> list[dict[str,str]]:
 
 def search_query(search_query: str, category: str):
     sites: list[dict[str,str]] = search_sites_by_category(category)
+    passed_sites: list[str] = []
     
     for site in sites:
-        print(site["search_url"])
+        search_url: str = site["search_url"]
+        search_url = search_url.format(query=search_query)
+        
+        try:
+            RESPONSE: requests.Response = requests.get(search_url, allow_redirects=False)
+            requests.session().max_redirects = 100
+
+            print(search_url, RESPONSE.status_code)
+
+            if RESPONSE.status_code:
+                passed_sites.append(search_url)
+
+        except SSLError:
+            pass
+    
+    return passed_sites
 
 
 if __name__ == "__main__":
     # print(parse_json("websites.json"))
     # print(search_sites_by_category(category="all"))
-    search_query(search_query="math", category="math")
+    print(search_query(search_query="math", category="math"))
 
